a338d2cbbef87bc41fa6e2fbfcc21f833f0e6484
dovanphuoc <dovanphuocht@gmail.com>
2022-10-21 14:25:49 -0700

    customize slider

--- khampha.js
+++ khampha.js
@@ -1,17 +1,30 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+const arrowLeft = '<i class="fa-solid fa-arrow-left"></i>'
+const arrowRight = '<i class="fa-solid fa-arrow-right"></i>'
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 10,
+    nav:true,
+    autoplay:true,
+    dots: false,
+    navText: [
+      arrowLeft, 
+      arrowRight
+    ],
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
+})
